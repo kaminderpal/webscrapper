79526c39d970ca92148452915fd75ab9e2d709d0
Kamy <kaminderpal@yahoo.com>
2020-01-14 22:06:52 -0500

    add cluster forking in server js

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express');
-const app = express();
-const axios = require('axios');
+const os = require('os');
+const cluster = require('cluster');
 const config = require('./config');
 const { PORT } = config;
 const scrapper = require('./src/routes/Amazon/scrapper');
+const mysqlConnection = require('./src/database/connect');
+const clusters = os.cpus().length;
 
-//routes
-app.use('/search', scrapper);
-app.use('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+if (cluster.isMaster) {
+  console.log('Master cluster setting up ' + clusters + ' workers...');
+  for (let i = 0; i < clusters; i++) {
+    cluster.fork();
+  }
+  cluster.on('online', function(worker) {
+    console.log('Worker ' + worker.process.pid + ' is online');
+  });
+  cluster.on('exit', function(worker) {
+    console.log('Worker', worker.process.pid, ' has exitted.');
+    console.log('Satrting new worker');
+    cluster.fork();
+  });
+} else {
+  const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is listening at ${PORT}`);
-});
+  //routes
+  app.use('/search', scrapper);
+  app.use('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+  app.listen(PORT, function() {
+    console.log(
+      `Express server listening on port ${PORT} and worker ${process.pid}`
+    );
+  });
+}
